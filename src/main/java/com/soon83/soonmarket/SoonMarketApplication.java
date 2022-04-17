9e634b1a85ba17028c48badc9de7c408bf2dacbd
ds <2601948@gmail.com>
2022-04-17 17:59:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/soon83/soonmarket/SoonMarketApplication.java
@@ -0,0 +1,13 @@
+package com.soon83.soonmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoonMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soon83/soonmarket/SoonMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon83.soonmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoonMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
